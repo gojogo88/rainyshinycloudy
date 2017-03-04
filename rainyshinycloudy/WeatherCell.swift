d3a6f3c36d03a03fa7aacbe794a61fb268fd3965
Jonathan Go <jogo88@gmail.com>
2017-03-04 11:34:38 +0900

    loaded previous version

---
+++ rainyshinycloudy/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  rainyshinycloudy
+//
+//  Created by Jonathan Go on 2017/03/04.
+//  Copyright © 2017 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
